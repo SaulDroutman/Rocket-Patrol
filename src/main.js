c463f0972d440597fc89a624a3a59660e9166a6e
SaulDroutman <128657309+SaulDroutman@users.noreply.github.com>
2024-01-22 12:46:23 -0800

    rocket movement

--- src/main.js
+++ src/main.js
@@ -6,5 +6,8 @@ let config = {
   }
 let game = new Phaser.Game(config)
 
+//setting global variables
 let borderUISize = game.config.height / 15
 let borderPadding = borderUISize / 3
+
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,40 @@
+class Rocket extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame)
+
+        //add to scene display and update
+        scene.add.existing(this)
+        
+        //set up vars
+        this.isFiring = false
+        this.moveSpeed = 2
+    }
+
+    update(){
+        //left and right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+                this.x -=this.moveSpeed
+            } else if (keyRIGHT.isDown && this.x <=game.config.width - borderUISize - this.width){
+                this.x += this.moveSpeed
+            }
+        }
+
+        //firing
+        if(Phaser.Input.Keyboard.JustDown(keyFIRE)){
+            this.isFiring = true
+        }
+        //move up on fire
+        if(this.isFiring && this.y >= borderUISize *3 + borderPadding){
+            this.y -= this.moveSpeed
+        }
+        //reset
+        if(this.y<=borderUISize*3 + borderPadding){
+            this.isFiring = false
+            this.y = game.config.height - borderUISize - borderPadding
+        }
+
+
+
+    }
+}
